4b820b443e1a2fabc688052fbd075437580d175a
luchetu <humluchetu@gmail.com>
2019-10-29 00:39:39 +0300

    fixed the bug on store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
+import {createStore,applyMiddleware} from 'redux';
 import logger from 'redux-logger';
-
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
-
+const middlewares=[logger];
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
+    middlewares.push(logger);
+  }
+const store=createStore(rootReducer,applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export const persistor = persistStore(store);
-
-export default { store, persistStore };
+export default store;
